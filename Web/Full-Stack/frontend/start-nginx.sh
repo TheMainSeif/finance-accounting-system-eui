a6cd157942c691902409128e89e908c7bebb9bbc
Seif <mseif634@gmail.com>
2025-12-24 08:13:47 +0200

    Deploy

--- Web/Full-Stack/frontend/start-nginx.sh
+++ Web/Full-Stack/frontend/start-nginx.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 # Replace placeholders in nginx.conf template
-echo "Configuring Nginx to listen on port ${PORT:-80} and proxy to ${BACKEND_URL}:${BACKEND_PORT:-5000}..."
+echo "Configuring Nginx to listen on port ${PORT:-80} and proxy to ${BACKEND_URL:-http://localhost:5000}..."
 sed -i "s!{{PORT}}!${PORT:-80}!g" /etc/nginx/conf.d/default.conf
-sed -i "s!{{BACKEND_URL}}!${BACKEND_URL}!g" /etc/nginx/conf.d/default.conf
-sed -i "s!{{BACKEND_PORT}}!${BACKEND_PORT:-5000}!g" /etc/nginx/conf.d/default.conf
+sed -i "s!{{BACKEND_URL}}!${BACKEND_URL:-http://localhost:5000}!g" /etc/nginx/conf.d/default.conf
 
 # Start nginx
 exec nginx -g "daemon off;"
